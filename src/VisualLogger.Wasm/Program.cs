23eced86f834d91e260ee54e5b06fb22dcbd9d52
Jim.Jiang <jim.jiang@ringcentral.com>
2022-03-21 12:02:07 +0800

    support .net5

--- src/VisualLogger.Wasm/Program.cs
+++ src/VisualLogger.Wasm/Program.cs
@@ -1,3 +1,33 @@
+#if NET5_0
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using VisualLogger.Wasm;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddBootstrapBlazor();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
+
+#else
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using VisualLogger.Wasm;
@@ -10,3 +40,5 @@ builder.Services.AddBootstrapBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+#endif
