e5e4c6be05fbd418fb015e072498899c78538ca7
Jim.Jiang <jim.jiang@ringcentral.com>
2022-06-16 10:57:38 +0800

    add wpf

---
+++ src/VisualLogger.WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VisualLogger.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddVisualLogger();
+#if DEBUG
+            serviceCollection.AddBlazorWebViewDeveloperTools();
+#endif
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
